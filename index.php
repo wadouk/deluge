463674ccd8e49d1b11642f441546c2af048f5f04
Nicolas Bétheuil <wadouk@gmail.com>
2011-07-23 12:31:27 +0200

    changement du framework de graphique implémentation des 3 types de graphique

--- index.php
+++ index.php
@@ -19,19 +19,24 @@
 			</form>
 		</div>
 		<h1>Le suivi de l'eau du jardin Santerre</h1>
+		<div style="height: 200px">
+		<div id="gtotal" class="graph"></div>
+		<div id="gweek"  class="graph" ></div>
+		<div id="gjardinier"  class="graph" ></div>
+		</div>
 			<ul>
 				<li>Si nous continuons à ce rythme de consommation, elle sera à la fin de l'année de  <span id="v_to_end"></span> litres</sup></li>
-				<li id="li_nb_jours_last">Il s'est déjà écoulé <span id="nb_jours_last"></span> jours depuis le dernier relevé, nourrissez moi svp</li>
+				<li id="li_nb_jours_last">Il s'est déjà écoulé <span id="nb_jours_last"></span> jours depuis le dernier relevé, merci d'en faire un</li>
 			</ul>
-			<p>S’il ne pleut pas, un à deux arrosages par semaine d’un arrosoir pourrait faire l’affaire si on bine et si on paille.</p>
-		<h1>Liste des relevés 
+			<p>S’il ne pleut pas, en binant ou en paillant, un à deux arrosoirs par semaine pourrait faire l’affaire </p>
+		
+			<h1>Liste des relevés 
 		<button id="add">Saisie d'un nouveau relevé</button>
 		</h1>
 		<table border="1" width="90%" align="center">
 			<thead>
 				<th>Date</th>
 				<th>Volume</th>
-				<th>Ecart depuis le dernier relevé <br>(en litres)</th>
 				<th>Consommation<br>(en litres/sem)</th>
 				<th>Par co-jardinier <br>(en litres/sem/parcelle)</th>
 				<th>En arrosoir <br>(4.5l/sem/parcelle)</th>
@@ -41,20 +46,10 @@
 			</tbody>
 		</table>
 		
-		<div id="gtotal" style="float:left">
-		
-		</div>
-		
-		<div id="gweek" style="float:left">
-		
-		</div>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js" type="text/javascript"></script>
 			<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js" type="text/javascript" ></script>
-			<script src="raphael-min.js" type="text/javascript" charset="utf-8"></script>
-			<script src="g.raphael-min.js" type="text/javascript" charset="utf-8"></script>
-      <script src="g.line-min.js" type="text/javascript" charset="utf-8"></script>
-			<script src="g.bar-min.js" type="text/javascript" charset="utf-8"></script>
 			<script src="date.js"  type="text/javascript" charset="utf-8"></script>
+			<script src="jquery.flot.min.js"  type="text/javascript" charset="utf-8"></script>
 			<script src="jshashtable-2.1.js" type="text/javascript" charset="utf-8"></script>
 			<script src="jquery.numberformatter-1.2.2.js"  type="text/javascript" charset="utf-8"></script>
       <script src="jquery.ui.datepicker-fr.js" type="text/javascript"></script>
--- santerre.js
+++ santerre.js
@@ -1,4 +1,3 @@
-
 $(function() {
 	$("button").button();
 	$("#li_nb_jours_last").hide();
@@ -43,41 +42,45 @@ $(function() {
 		return (val == null? newval: val);
 	}
 	var list_releves = [];
-	var x_total = [], y_total = [], y_week = [];
+	var points_week = [], points_total= [], points_jardinier = [];
 	
 	function prix() {
 		$.ajax({
 			url: 'prix.php',
 			success: function (data) {
+				data = data[0];
 				
-		try {
-			data = data[0];
-			
-			var v_to_end = 	list_releves[0].v_par_time*data.toend;
-	
-			y_total.push(v_to_end);
-			v_to_end = $.formatNumber(v_to_end,{locale:"fr"});
-			$("#v_to_end").text( v_to_end);
-			if (list_releves[0].nb_jours_last<-7) {
-				$("#nb_jours_last").text(-list_releves[0].nb_jours_last);
-				$("#li_nb_jours_last").show();
-			}
-			x_total.push(Date.parseString(nvl(data.end,"&nbsp;"),'yyyy-MM-dd').getTime());
-	
-			console.log(x_total);
-			
-			console.log(y_total);
-			var r = Raphael("gtotal");
-			r.g.text(160, 10, "Voulmes actuels et prévisions");
-			var lines = r.g.linechart(10,10,300,220, [x_total],[y_total]);
-			lines.axis[0].text = "ici";
-			lines.axis[1].text = "la";
-		}	catch (e) {
-			console.error(e);
-		}
+				var v_to_end = 	list_releves[0].v_par_time*data.toend;
+		
+				points_total.push([Date.parseString(nvl(data.end,"&nbsp;"),'yyyy-MM-dd').getTime(), v_to_end]);
+				var gtotal = $("#gtotal");
+				var plot = $.plot(gtotal, [{label:"Volume",data:points_total}], { xaxis: { mode: "time", 
+						monthNames: months } });
+				o = plot.pointOffset({ x: Date.parseString(nvl(list_releves[list_releves.length-1].dafter,"&nbsp;"),'yyyy-MM-dd').getTime(), y: v_to_end});
+				gtotal.append('<div style="position:absolute;left:' + (o.left + 4) + 'px;top:' + o.top + 'px;color:#666;font-size:smaller">Mesuré</div>');
+				
+				o = plot.pointOffset({ x: Date.parseString(nvl(list_releves[0].dafter,"&nbsp;"),'yyyy-MM-dd').getTime(), y: v_to_end});
+				gtotal.append('<div style="position:absolute;left:' + (o.left + 4) + 'px;top:' + o.top + 'px;color:#666;font-size:smaller">Prévisions</div>');
+				var ctx = plot.getCanvas().getContext("2d");
+				ctx.beginPath();
+				o.left += 4;
+				ctx.moveTo(o.left, o.top);
+				ctx.lineTo(o.left, o.top - 10);
+				ctx.lineTo(o.left + 10, o.top - 5);
+				ctx.lineTo(o.left, o.top);
+				ctx.fillStyle = "#000";
+				ctx.fill();
+
+				v_to_end = $.formatNumber(v_to_end,{locale:"fr"});
+				$("#v_to_end").text( v_to_end);
+				if (list_releves[0].nb_jours_last<-7) {
+					$("#nb_jours_last").text(-list_releves[0].nb_jours_last);
+					$("#li_nb_jours_last").show();
+				}
 			}
 		});
 	}
+	var months = ["Jan","Fev","Mar","Avr","Mai","Juin", "Juil", "Aout", "Sept", "Oct", "Nov", "Dec"];
 	function refresh() {
 		$.ajax({
 			url: 'list.php',
@@ -91,30 +94,31 @@ $(function() {
 						var v_par_time = data[line].vdiff / data[line].ddiff;
 						data[line].v_par_time =v_par_time;
 					}
-					x_total.unshift(Date.parseString(nvl(data[line].dafter,"&nbsp;"),'yyyy-MM-dd').getTime());
-					y_total.unshift(Math.round(data[line].vafter));
-					y_week.unshift(Math.round(data[line].v_par_time));
-					try {
-						$("#table_content").append(
-						"<tr>"+
-						"<td>"+Date.parseString(nvl(data[line].dafter,"&nbsp;"),'yyyy-MM-dd').format('dd/MM/yy')+"</td>"+
-						"<td class='numeric'>"+round(data[line].vafter)+"</td>"+
-						"<td class='numeric'>"+round(data[line].vdiff)+"</td>"+
-						"<td class='numeric'>"+round(v_par_time)+"</td>"+
-						"<td class='numeric'>"+ round(v_par_time/46)+"</td>"+
-							"<td class='numeric'>"+round(v_par_time/46/4.5)+"</td>"+
-						"</tr>");
-					} catch (e) {
-						console.error(e);
-					}
+					points_total.unshift([Date.parseString(nvl(data[line].dafter,"&nbsp;"),'yyyy-MM-dd').getTime(), Math.round(data[line].vafter)]);
+					points_week.unshift([Date.parseString(nvl(data[line].dafter,"&nbsp;"),'yyyy-MM-dd').getTime(), Math.round(data[line].v_par_time)]);
+					points_jardinier.unshift([Date.parseString(nvl(data[line].dafter,"&nbsp;"),'yyyy-MM-dd').getTime(), Math.round(data[line].v_par_time/46)]);
+					$("#table_content").append(
+					"<tr>"+
+					"<td>"+Date.parseString(nvl(data[line].dafter,"&nbsp;"),'yyyy-MM-dd').format('dd/MM/yy')+"</td>"+
+					"<td class='numeric'>"+round(data[line].vafter)+"</td>"+
+					"<td class='numeric'>"+round(v_par_time)+"</td>"+
+					"<td class='numeric'>"+ round(v_par_time/46)+"</td>"+
+						"<td class='numeric'>"+round(v_par_time/46/4.5)+"</td>"+
+					"</tr>");
+					
 				}
-				
-				
-			var w = Raphael("gweek");
-			w.g.text(160, 10, "Consommation (en litres/sem)");
-			console.log(x_total);
-			console.log(y_week);
-			var lines = w.g.barchart(10,10,300,220,[y_week]);
+				$.plot($("#gweek"), [{label:"Consommation",data:points_week}], 
+					{ 
+					xaxis: { 
+						mode: "time" ,
+						monthNames: months,
+						timeformat: "%d %b"
+					}, 
+					yaxis: {min: 0} 
+					});
+				$.plot($("#gjardinier"), [{label:"Par co-jardinier",data:points_jardinier}], { xaxis: { mode: "time", 
+						monthNames: months,
+						timeformat: "%d %b"}, yaxis: {min: 0} });
 				prix();
 			}
 		});
